26a68bde53b1ee768bc4f01d013767aa2493df11
dipzcoded <dipoakerele@gmail.com>
2022-09-04 19:10:05 +0100

    almost done adding scrollreveal

--- app.js
+++ app.js
@@ -35,3 +35,56 @@ allNavLinks.forEach((el) => {
 window.addEventListener("scroll", () => {
   headerTag.classList.toggle("sticky", window.scrollY > 0);
 });
+
+window.addEventListener("load", () => {
+  headerTag.classList.toggle("sticky", window.scrollY > 0);
+});
+
+// scrollreveal
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+// showcase section
+sr.reveal(".showcase-text h2");
+sr.reveal(".showcase-text h1", { delay: 300 });
+sr.reveal(".showcase-text > p", { delay: 500 });
+sr.reveal(".showcase-btn-flex", { delay: 800 });
+sr.reveal(".showcase-flex .showcase-img", { delay: 500 });
+
+// category section
+sr.reveal(".category .category-smallheading");
+sr.reveal(".category .category-main", { delay: 400 });
+sr.reveal(".category .category-containerflex .category-flexitem.flexitem1", {
+  delay: 600,
+});
+sr.reveal(".category .category-containerflex .category-flexitem.flexitem2", {
+  delay: 900,
+});
+sr.reveal(".category .category-containerflex .category-flexitem.flexitem3", {
+  delay: 1200,
+});
+sr.reveal(".category .category-containerflex .category-flexitem.flexitem4", {
+  delay: 1500,
+});
+
+// destination section
+sr.reveal(".destination .destination-smallheading", { delay: 400 });
+sr.reveal(".destination .destination-main", { delay: 800 });
+sr.reveal(
+  ".destination .destination-containerflex .destination-flexitem.destflex-1",
+  { delay: 1200 }
+);
+sr.reveal(
+  ".destination .destination-containerflex .destination-flexitem.destflex-2",
+  { delay: 1500 }
+);
+sr.reveal(
+  ".destination .destination-containerflex .destination-flexitem.destflex-3",
+  { delay: 1800 }
+);
+
+// book section
